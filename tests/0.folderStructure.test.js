b37c31adf334a81cc8fd9bf437bcb49a5e8e760e
Juilfsjc <64669923+Juilfsjc@users.noreply.github.com>
2022-09-25 18:34:25 -0400

    Finished Project (I think)

--- tests/0.folderStructure.test.js
+++ tests/0.folderStructure.test.js
@@ -1,49 +1,64 @@
-const dirTree = require(`directory-tree`);
-const _ = require(`lodash`);
+/**
+ * @jest-environment jsdom
+*/
+/* eslint-env browser */
 
-describe(`folder structure`, function () {
-  test(`root directory is properly setup`, () => {
-    const rootTree = dirTree(`${__dirname}/..`);
-    const nodes = rootTree.children.map((node) => node.name);
+const fs = require(`fs`);
+const path = require(`path`);
 
-    const expectedNodes = [
-      `.git`,
-      `.github`,
-      `.vscode`,
-      `node_modules`,
-      `resources`,
-      `tests`,
-      `.babelrc`,
-      `.eslintrc.json`,
-      `.gitignore`,
-      `.prettierignore`,
-      `.prettierrc`,
-      `jest.config.js`,
-      `index.html`,
-      `package.json`,
-      `README.md`,
+const html = fs.readFileSync(path.resolve(__dirname, `../index.html`), `utf8`);
+
+jest.dontMock(`fs`);
+
+describe(`html content`, function () {
+  beforeEach(() => {
+    document.documentElement.innerHTML = html.toString();
+  });
+
+  afterEach(() => {
+    // restore the original func after test
+    jest.resetModules();
+  });
+
+  test(`page title is IT3049 Chat`, function () {
+    const documentTitle = document.title;
+
+    expect(documentTitle).toContain(`IT3049 Chat`);
+  });
+
+  it(`contains all the starting links and styles in head`, function () {
+    const styleElements = document.querySelectorAll(`head > link`);
+    const styleSources = Array.from(styleElements)
+      .filter((ele) => ele.rel === `stylesheet`)
+      .map((ele) => ele.href.slice(17));
+    const expectedStyles = [
+      `resources/vendor/bootstrap.min.css`,
+      `resources/vendor/fontawesome-free/css/all.min.css`,
+      `resources/css/styles.css`
     ];
-    expect(nodes).toEqual(expect.arrayContaining(expectedNodes));
+    expect(styleSources).toEqual(expect.arrayContaining(expectedStyles));
   });
 
-  test(`resources directory is properly setup`, () => {
-    const resourcesTree = dirTree(`${__dirname}/../resources`);
-    const nodes = resourcesTree.children.map((node) => node.name);
-    const expectedNodes = [ `vendor`, `css`, `js` ];
-    expect(nodes).toEqual(expect.arrayContaining(expectedNodes));
+  it(`contains all the starting scripts in head`, function () {
+    const scriptElements = document.querySelectorAll(`head > script`);
+    const scriptSources = Array.from(scriptElements)
+      .map((ele) => ele.src.slice(17));
+    const expected = [
+      `resources/vendor/bootstrap.min.js`,
+      `resources/vendor/popper.min.js`,
+      `resources/vendor/jquery-3.5.1.slim.min.js`
+    ];
+    expect(scriptSources).toEqual(expect.arrayContaining(expected));
   });
 
-  test(`scripts directory includes one index.js files`, () => {
-    const scriptsTree = dirTree(`${__dirname}/../resources/js`);
-    const nodes = scriptsTree.children.map((node) => node.name);
-
-    expect(nodes).toContain(`index.js`);
+  it(`contains all the custom scripts in body`, function () {
+    const scriptElements = document.querySelectorAll(`body > script`);
+    const scriptSources = Array.from(scriptElements)
+      .map((ele) => ele.src.slice(17));
+    const expected = [
+      `resources/js/index.js`,
+    ];
+    expect(scriptSources).toEqual(expect.arrayContaining(expected));
   });
 
-  test(`styles directory includes one styles.css files`, () => {
-    const stylesTree = dirTree(`${__dirname}/../resources/css`);
-    const nodes = stylesTree.children.map((node) => node.name);
-
-    expect(nodes).toContain(`styles.css`);
-  });
 });
