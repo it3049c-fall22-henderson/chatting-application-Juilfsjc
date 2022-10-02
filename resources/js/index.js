691fde24fdfc6eaf2ffd3a15f715ac3676d40d04
Juilfsjc <64669923+Juilfsjc@users.noreply.github.com>
2022-10-02 17:09:32 -0400

    removed dark mode, couldnt get it to work

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,28 +1,65 @@
 const nameInput = document.getElementById("my-name-input");
 const myMessage = document.getElementById("my-message");
 const sendButton = document.getElementById("send-button");
-const saveButton = document.getElementById("saveButton");
+const saveButton = document.getElementById("save-button");
 const chatBox = document.getElementById("chat");
+const serverURL = `https://it3049c-chat-application.herokuapp.com/messages`;
 
-async function updateMessages() {
-  // Fetch Messages
+updateMessages();
+
+const MILLISECONDS_IN_TEN_SECONDS = 10000;
+setInterval(updateMessages, MILLISECONDS_IN_TEN_SECONDS);
+
+sendButton.addEventListener("click", function(sendButtonClickEvent) {
+  
+  sendButtonClickEvent.preventDefault();
+  const sender = nameInput.value;
+  const message = myMessage.value;
+
+  sendMessages(sender,message);
+  myMessage.value = "";
+
+});
+
+saveButton.addEventListener("click", function(saveButtonClickEvent) {
+    saveButtonClickEvent.preventDefault();
+
+    if(nameInput.value != ""){
+        myMessage.removeAttribute("disabled");
+    }
+    else {
+        myMessage.setAttribute("disabled", "disabled");
+    }
+    
+    localStorage.setItem("name", nameInput.value);
+});
+
+function updateMessagesInChatBox(){
+
+  updateMessages();
+
+}
+
+
+function fetchMessages() {
+
+    return fetch(serverURL)
+        .then( response => response.json());
+
+}
+
+async function updateMessages(){
+
   const messages = await fetchMessages();
-  // Loop over the messages. Inside the loop we will:
-      // get each message
-      // format it
-      // add it to the chatbox
+
   let formattedMessages = "";
+
   messages.forEach(message => {
       formattedMessages += formatMessage(message, nameInput.value);
   });
+
   chatBox.innerHTML = formattedMessages;
-}
 
-const serverURL = `https://it3049c-chat-application.herokuapp.com/messages`;
-
-async function fetchMessages() {
-    return fetch(serverURL)
-        .then( response => response.json())
 }
 
 function formatMessage(message, myNameInput) {
@@ -59,7 +96,7 @@ function sendMessages(username, text) {
       sender: username,
       text: text,
       timestamp: new Date()
-  }
+  };
 
   fetch (serverURL, {
       method: `POST`, 
@@ -69,36 +106,3 @@ function sendMessages(username, text) {
       body: JSON.stringify(newMessage)
   });
 }
-
-sendButton.addEventListener("click", function(sendButtonClickEvent) {
-  sendButtonClickEvent.preventDefault();
-  const sender = nameInput.value;
-  const message = myMessage.value;
-
-  sendMessages(sender,message);
-  myMessage.value = "";
-});
-
-saveButton.addEventListener("click", function(saveButtonClickEvent) {
-    saveButtonClickEvent.preventDefault();
-
-    if(nameInput.value != "")
-    {
-        myMessage.removeAttribute("disabled");
-    }
-    else
-    {
-        myMessage.setAttribute("disabled", "disabled");
-    }
-
-    localStorage.setItem("name", nameInput.value);
-
-});
-
-function switchModesLightDark() {
-    var element = document.body;
-    element.classList.toggle("darkMode");
-}
-
-const MILLISECONDS_IN_TEN_SECONDS = 10000;
-setInterval(updateMessages, MILLISECONDS_IN_TEN_SECONDS);
